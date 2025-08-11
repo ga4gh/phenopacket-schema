01be197870a616435ee5820d5fba6e88e2783d50
Daniel Danis <daniel.gordon.danis@protonmail.com>
2025-08-11 10:46:25 +0200

    Relax protobuf version in Python (#393)
    * Add Python test data files.
    
    * Allow using protobuf `<=5.2.9` in Python.
    
    * Allow using protobuf `<7.0.0`.

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -3,8 +3,29 @@ import os
 import pytest
 
 
+@pytest.fixture(scope="session")
+def fpath_python_src_dir(
+    fpath_test_dir: str,
+) -> str:
+    return os.path.realpath(
+        os.path.join(
+            fpath_test_dir,
+            os.pardir,
+        )
+    )
+
+
 @pytest.fixture(scope='session')
 def fpath_test_dir() -> str:
     # When running `pytest` from the top-level Python folder (`phenopacket-schema/python`)
     # this path will evaluate to the `tests` folder.
     return os.path.dirname(__file__)
+
+
+@pytest.fixture(scope="session")
+def fpath_test_data_dir(
+    fpath_python_src_dir: str,
+) -> str:
+    # When running `pytest` from the top-level Python folder (`phenopacket-schema/python`)
+    # this path will evaluate to the `data` folder.
+    return os.path.join(fpath_python_src_dir, "data")
